6a7f400a9c5d440eaaf4a76818b66dfb3b58b23e
Mitch Myburgh <mitchmyburgh@gmail.com>
2015-08-31 18:32:20 +0200

    started listing tokens

---
+++ lex_ula.py
@@ -0,0 +1,19 @@
+#----------------------------------------------
+# lex_ula.py
+#
+# Lexical Analyser for the ula (unconventional language)
+#----------------------------------------------
+
+#List of token names
+tokens = (
+    'ID',
+    'FLOAT_LITERAL',
+    '@', #addition
+    '$', #subtration
+    '#', #multiplication
+    '&', #division
+    '=',
+    '(',
+    ')',
+    'COMMENT'
+)
